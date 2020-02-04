173e323564789c2a45281d2e9cc1a8a05f305859
Devansh Mittal <demi1671@colorado.edu>
2020-02-03 18:00:09 -0800

    Added functions to divide ciphertext into blocks and to verify padding

--- Project-1-Cryptography/Padding_Oracle/padding_oracle.py
+++ Project-1-Cryptography/Padding_Oracle/padding_oracle.py
@@ -2,6 +2,33 @@
 
 import requests
 import sys
+from Crypto.Cipher import AES
+
+oracle_url = ""
+
+def divide_ciphertext(ciphertext):
+    """ Divides ciphertext into blocks of 16 bytes """
+    if len(ciphertext) % AES.block_size:
+        print("Invalid length")
+    else:
+        cipher_blocks = []
+
+        for i in range(0,len(ciphertext),AES.block_size*2):
+            cipher_blocks.append(ciphertext[i:i+(AES.block_size*2)])
+
+        #print(cipher_blocks)
+        return cipher_blocks
+
+def verify_padding(ciphertext):
+    """ Verify whether padding is correct """
+    r = requests.get("%s?message=%s" % (oracle_url, bytes.fromhex(ciphertext).hex()))
+    r.raise_for_status()
+    obj = r.json()
+
+    if obj['status'] == 'invalid_padding':
+        return 0
+    else:
+        return 1
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
@@ -10,9 +37,13 @@ if __name__ == '__main__':
     oracle_url = sys.argv[1]
     ciphertext = sys.argv[2]
 
+    divide_ciphertext(ciphertext)
+    #dec_oracle_route()
     # Example check of ciphertext at the oracle URL:
+    """
     r = requests.get("%s?message=%s" % (oracle_url, bytes.fromhex(ciphertext).hex()))
     r.raise_for_status()
     obj = r.json()
-    print(obj[status])
-
+    print(AES.block_size)
+    print(obj)
+    """
