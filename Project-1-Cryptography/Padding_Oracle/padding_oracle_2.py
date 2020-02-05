9d0fd7b8a3aa0fe488c33df0d2a5115006d5507b
Devansh Mittal <demi1671@colorado.edu>
2020-02-05 15:49:07 -0800

    Added logic to decrypt all blocks

--- Project-1-Cryptography/Padding_Oracle/padding_oracle_2.py
+++ Project-1-Cryptography/Padding_Oracle/padding_oracle_2.py
@@ -39,57 +39,59 @@ def verify_padding(ciphertext):
 
 def decrypt_last_block(cipher_blocks):
 
-    cipher_blocks_dup = cipher_blocks
-    last_block = cipher_blocks[-1]
-    second_last = cipher_blocks[-2]
-    cipher_bytes = bytearray.fromhex(cipher_blocks[-2])
-    decrypted_block = []
-    plaintext_block = []
-    verify_plaintext = []
+    decrypted_text = ""
 
-    i = 1
-    while i < 17:
-        for k in range(1,i):
-            cipher_bytes[-k] = i ^ decrypted_block[-k]
-        for j in range(1,256):    
-            cipher_bytes[-i] = j
-            print(cipher_bytes.hex())
-            #cipher_hex = hex(int(cipher_bytes.hex(),16) ^ int(last_block,16))
-            
-            cipher_blocks_dup[-2] = cipher_bytes.hex()
-            ciphertext = ""
-            for ele in cipher_blocks_dup:
-                ciphertext += ele
-            if verify_padding(ciphertext) == 1:
-                print(cipher_blocks_dup[-2])
-                print(j)
-                break;
-
-        decrypted_block.insert(0,(j ^ i))
-        print("decrypted_block: ", decrypted_block)
-        plaintext_block.insert(0,(decrypted_block[-i] ^ bytearray.fromhex(second_last)[-i]))
-        print("plaintext: ", plaintext_block)
-
-        if i == 1:
-            for l in range((i+1),(plaintext_block[-i]+1)):
-                decrypted_block.insert(0,(bytearray.fromhex(second_last)[-l] ^ plaintext_block[-i]))
-                plaintext_block.insert(0,plaintext_block[-i])
-            i = plaintext_block[-1] + 1
+    for z in range(1,len(cipher_blocks)):
+        if z == 1:
+            cipher_blocks_dup = cipher_blocks
         else:
-            i = i+1
+            cipher_blocks_dup = cipher_blocks[:-(z-1)]
+        prev_block = cipher_blocks[-(z+1)]
+        cipher_bytes = bytearray.fromhex(cipher_blocks[-(z+1)])
+        decrypted_block = []
+        plaintext_block = []
 
-    #Verify if plaintext xor decrypted block is equal to previous cipher block
-    for a,b in zip(bytes(plaintext_block),bytes(decrypted_block)):
-        verify_plaintext.append(a^b)
+        i = 1
+        while i < 17:
+            for k in range(1,i):
+                cipher_bytes[-k] = i ^ decrypted_block[-k]
+            for j in range(0,256):    
+                cipher_bytes[-i] = j
+                print(cipher_bytes.hex())
+        
+                cipher_blocks_dup[-2] = cipher_bytes.hex()
+                ciphertext = ""
+                for ele in cipher_blocks_dup:
+                    ciphertext += ele
+                if verify_padding(ciphertext) == 1:
+                    print(j)
+                    break;
 
-    if verify_plaintext == list(bytearray.fromhex(second_last)):
-        print("success")
+            decrypted_block.insert(0,(j ^ i))
+            print("decrypted_block: ", decrypted_block)
+            plaintext_block.insert(0,(decrypted_block[-i] ^ bytearray.fromhex(prev_block)[-i]))
+            print("plaintext: ", plaintext_block)
 
-    print(decrypted_block)
-    #unpadded_block = unpad(bytes(plaintext_block).hex())
-    print(bytes(plaintext_block).hex())
-    #print(bytearray.fromhex(unpadded_block).decode())
+            if i == 1 and z == 1:
+                for l in range((i+1),(plaintext_block[-i]+1)):
+                    decrypted_block.insert(0,(bytearray.fromhex(prev_block)[-l] ^ plaintext_block[-i]))
+                    plaintext_block.insert(0,plaintext_block[-i])
+                i = plaintext_block[-1] + 1
+            else:
+                i = i+1
 
+        if z == 1:
+            unpadded_block = unpad(bytes(plaintext_block))
+        else:
+            unpadded_block = bytes(plaintext_block)
+
+        decrypted_text = unpadded_block.hex() + decrypted_text 
+        print("Block",z,":",unpadded_block.hex())
+
+    print(decrypted_text)
+    actual_text = decrypted_text[:-64]
+    actual_text_ascii = bytearray.fromhex(actual_text).decode()
+    print(actual_text_ascii)
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
@@ -101,12 +103,3 @@ if __name__ == '__main__':
     cipher_blocks = divide_ciphertext(ciphertext)
 
     decrypt_last_block(cipher_blocks)
-    #dec_oracle_route()
-    # Example check of ciphertext at the oracle URL:
-    """
-    r = requests.get("%s?message=%s" % (oracle_url, bytes.fromhex(ciphertext).hex()))
-    r.raise_for_status()
-    obj = r.json()
-    print(AES.block_size)
-    print(obj)
-    """
